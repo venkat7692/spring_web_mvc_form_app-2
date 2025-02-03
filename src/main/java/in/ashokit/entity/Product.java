3617217ea48f104774648cdcdb41840eaa5b09c1
venkat <venkatgeethsai@gmail.com>
2025-02-03 14:37:17 +0530

    first commmit

---
+++ src/main/java/in/ashokit/entity/Product.java
@@ -0,0 +1,37 @@
+package in.ashokit.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+private Integer productId;
+private String ProductName;
+private Double productPrice;
+
+public Integer getProductId() {
+	return productId;
+}
+public void setProductId(Integer productId) {
+	this.productId = productId;
+}
+public String getProductName() {
+	return ProductName;
+}
+public void setProductName(String productName) {
+	ProductName = productName;
+}
+public Double getProductPrice() {
+	return productPrice;
+}
+public void setProductPrice(Double productPrice) {
+	this.productPrice = productPrice;
+}
+@Override
+public String toString() {
+	return "Product [productId=" + productId + ", ProductName=" + ProductName + ", productPrice=" + productPrice + "]";
+}
+
+
+}
